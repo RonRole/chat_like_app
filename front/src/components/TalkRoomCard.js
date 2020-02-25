6d13ea9afe7ecfe385c17966dea007dcd85de67c
RonRole <ronrolenewadress@gmail.com>
2020-02-25 02:17:53 -0800

    TalkRoomPageを小さくしている途中

--- front/src/components/TalkRoomCard.js
+++ front/src/components/TalkRoomCard.js
@@ -4,6 +4,19 @@ import { Link } from "react-router-dom"
 
 class TalkRoomCard extends React.Component {
     render() {
+        if(this.props.readOnly) {
+            return (
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>{this.props.description}</Card.Text>
+                        <Row className="d-md-flex justify-content-end">
+                            <Link className="btn btn-primary mr-2" to={`/talk_rooms/${this.props.id}`}>入る</Link>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            )
+        }
         return (
             <Card>
                 <Card.Body>
--- front/src/containers/TalkRoomPage.js
+++ front/src/containers/TalkRoomPage.js
@@ -7,6 +7,42 @@ import TalkRoomAddingForm from '../components/TalkRoomAddingForm'
 import { getTalkRoomIds } from '../modules/talkRoomModule/TalkRoomReducer'
 import TalkRoomModule from '../modules/talkRoomModule/TalkRoomModule'
 
+class TalkRoomsArea extends React.Component {
+
+    render() {
+        return (
+            <TransitionGroup className="row">
+                {this.props.talkRoomIds.map((talkRoomId,index) => {
+                    return(
+                        <CSSTransition key = {index} timeout={100} classNames="fade">
+                            <Col key={index} md={{span:4}} className="mt-2">
+                                <TalkRoomCard   className  ="col-md-4"
+                                                title      ={this.props.getTalkRoomById(talkRoomId).title} 
+                                                description={this.props.getTalkRoomById(talkRoomId).description}
+                                                id         ={talkRoomId}
+                                                key        ={index} 
+                                                destroy    ={this.props.destroyTalkRoom}
+                                                readOnly   ={this.props.readOnly}/>  
+
+                            </Col>
+                        </CSSTransition>
+                    )
+                })}
+            </TransitionGroup>
+        )
+    }
+}
+
+class TalkRoomAreaLabel extends React.Component {
+    render() {
+        return(
+            <h6 style={{borderBottom:"1px solid gray"}}>
+                <strong>{this.props.loginUser.name}</strong>{this.props.about}
+            </h6>
+        )
+    }
+}
+
 export class TalkRoomPage extends React.Component {
     state = {
         modalShow : false
@@ -17,48 +53,12 @@ export class TalkRoomPage extends React.Component {
     render() {
         return (
             <Container> 
-                <h6 style={{borderBottom:"1px solid gray"}}>
-                    <strong>{this.props.loginUser.name}</strong>さんが作成したトークルーム
-                </h6>
+                <TalkRoomAreaLabel loginUser={this.props.loginUser} about="さんが作成したトークルーム" />
                 <Button variant="primary" onClick={() => this.setState({modalShow:true})}>トークルームを追加</Button>
-                <TransitionGroup className="row">
-                    {this.props.ownRooms.map((talkRoomId,index) => {
-                        return(
-                            <CSSTransition key = {index} timeout={100} classNames="fade">
-                                <Col key={index} md={{span:4}} className="mt-2">
-                                    <TalkRoomCard   className  ="col-md-4"
-                                                    title      ={this.props.getTalkRoomById(talkRoomId).title} 
-                                                    description={this.props.getTalkRoomById(talkRoomId).description}
-                                                    id         ={talkRoomId}
-                                                    key        ={index} 
-                                                    destroy    ={this.props.destroyTalkRoom}/>  
+                <TalkRoomsArea {...this.props} talkRoomIds={this.props.ownRoomIds} />
 
-                                </Col>
-                            </CSSTransition>
-                        )
-                    })}
-                </TransitionGroup>
-                
-                <h6 style={{borderBottom:"1px solid gray"}}>
-                    <strong>{this.props.loginUser.name}</strong>さんが参加しているトークルーム
-                </h6>
-                <TransitionGroup className="row">
-                    {this.props.talkRoomIds.map((talkRoomId,index) => {
-                        return(
-                            <CSSTransition key = {index} timeout={100} classNames="fade">
-                                <Col key={index} md={{span:4}} className="mt-2">
-                                    <TalkRoomCard   className  ="col-md-4"
-                                                    title      ={this.props.getTalkRoomById(talkRoomId).title} 
-                                                    description={this.props.getTalkRoomById(talkRoomId).description}
-                                                    id         ={talkRoomId}
-                                                    key        ={index} 
-                                                    destroy    ={this.props.destroyTalkRoom}/>  
-
-                                </Col>
-                            </CSSTransition>
-                        )
-                    })}
-                </TransitionGroup>
+                <TalkRoomAreaLabel loginUser={this.props.loginUser} about="さんが参加しているトークルーム" />
+                <TalkRoomsArea {...this.props} talkRoomIds={this.props.joinedRoomIds} readOnly/>
 
                 <TalkRoomAddingForm onSubmit={({title,description})=>{
                         this.props.addTalkRoom({
@@ -83,9 +83,9 @@ const mapStateToProps = (state) => {
     return {
         loginUser       : state.logReducer.isLoggedIn,
         talkRooms       : state.talkRoomReducer.talkRooms,
-        talkRoomIds     : getTalkRoomIds(state.talkRoomReducer).filter(id => state.talkRoomReducer.talkRooms[id].author_id !== state.logReducer.isLoggedIn.id),
+        joinedRoomIds     : getTalkRoomIds(state.talkRoomReducer).filter(id => state.talkRoomReducer.talkRooms[id].author_id !== state.logReducer.isLoggedIn.id),
         getTalkRoomById : (id) => state.talkRoomReducer.talkRooms[id],
-        ownRooms        : getTalkRoomIds(state.talkRoomReducer).filter(id => state.talkRoomReducer.talkRooms[id].author_id === state.logReducer.isLoggedIn.id)
+        ownRoomIds        : getTalkRoomIds(state.talkRoomReducer).filter(id => state.talkRoomReducer.talkRooms[id].author_id === state.logReducer.isLoggedIn.id)
     }
 }
 
