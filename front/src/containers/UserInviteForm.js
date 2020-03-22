6dd12fe630e8be5b3c5e854f7eafe9224b7e7799
RonRole <ronrolenewadress@gmail.com>
2020-03-22 08:45:37 -0700

    ユーザーお誘い機能を作ろうとしている

--- front/src/containers/UserInviteForm.js
+++ front/src/containers/UserInviteForm.js
@@ -1,16 +1,27 @@
 import React from "react"
 import ModalForm from "../components/ModalForm"
 import { IdFormGroup, NameFormGroup } from "../components/UserFormGroups"
-import { Button } from "react-bootstrap"
+import { Button, Image } from "react-bootstrap"
+import { connect } from "react-redux"
 
 class UserInviteForm extends React.Component {
+
+    componentDidMount() {
+        console.log(this.props.loginUser)
+    }
 
     render() {
         return (
             <ModalForm
                 {...this.props}
                 header = {<strong>ユーザーを誘う</strong>}
-                body = {<div><IdFormGroup /><NameFormGroup/></div>}
+                body = {
+                    <div>
+                        <IdFormGroup />
+                        <NameFormGroup/>
+                        <Image src={`${process.env.REACT_APP_BACKEND_ADDRESS}/${this.props.loginUser.image.profile.url}`}/>
+                    </div>
+                }
                 footer = {
                     <div>
                         <Button className="mr-2" type="submit">さがす</Button>
@@ -22,4 +33,10 @@ class UserInviteForm extends React.Component {
     }
 }
 
-export default UserInviteForm
+const mapStateToProps = (state) => {
+    return {
+        loginUser : state.logStatus.isLoggedIn,
+    }
+}
+
+export default connect(mapStateToProps)(UserInviteForm)
