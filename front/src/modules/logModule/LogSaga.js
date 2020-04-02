e538bb4b3f3446cc94fc651ccbf9e6c13f30b895
RonRole <ronrolenewadress@gmail.com>
2020-04-02 14:32:17 -0700

    デプロイ

--- front/src/modules/logModule/LogSaga.js
+++ front/src/modules/logModule/LogSaga.js
@@ -7,7 +7,7 @@ import FormErrorActions from "../FormErrorModule/FormErrorActions"
 //saga
 export function* handleGetDefLoginStart(action) {
     const accessResult = yield call(DataAccessor.get, {
-        url : `${process.env.REACT_APP_BACKEND_ADDRESS}/login`
+        url : `https://${process.env.HEROKU_PRIVATE_IP}/login`
     })
 
     if(accessResult.isSuccess) {
