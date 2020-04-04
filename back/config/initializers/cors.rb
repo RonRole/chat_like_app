36081025bd5aa470d6c6d67e346aeb4555ba2efb
RonRole <ronrolenewadress@gmail.com>
2020-04-04 02:51:13 -0700

    sawai

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("http://$HOST_IP:$PORT") { 'sawai' }
+    origins ENV.fetch("REACT_APP_FRONTEND_ADDRESS") { 'sawai' }
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
