534538fd5045e5f8f6319c7c8c99ca1956c2d817
RonRole <ronrolenewadress@gmail.com>
2020-04-04 23:10:36 -0700

    sawai

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'chat-like-app.herokuapp.com'#ENV.fetch("REACT_APP_FRONTEND_ADDRESS"){'localhost:3000'}
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
