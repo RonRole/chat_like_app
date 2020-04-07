7533c716a75edc227ca81b837da36c2733ca9d26
ronrole <ronrolenewadress@gmail.com>
2020-04-07 18:29:18 +0900

    sawai

--- socket/socket_server.js
+++ socket/socket_server.js
@@ -1,4 +1,6 @@
 console.log("socket server started")
+console.log(`PORT:${process.env.SOCKET_PORT || 8000}`)
+console.log(`HOST:${process.env.SOCKET_HOST || 'localhost'}`)
 
 const http = require('http')
 const server = http.createServer();
