9ed3fa494d669f49500019aaadb034b418647aa1
ronrole <ronrolenewadress@gmail.com>
2020-04-07 17:15:39 +0900

    sawai

--- heroku-express.js
+++ heroku-express.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-//  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('/usr/src/front/build'))
 //  .set('views', path.join(__dirname, 'views'))
 //  .set('view engine', 'ejs')
   .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
