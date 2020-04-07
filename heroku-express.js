15e0499fc4b67ce6b4cc1c603edbb4728fca541f
ronrole <ronrolenewadress@gmail.com>
2020-04-07 16:20:13 +0900

    sawai

--- heroku-express.js
+++ heroku-express.js
@@ -3,11 +3,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+console.log(path.join(__dirname, 'build'))
+
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('KANDAKENGO!!!!'))
+  .get('/', (req, res) => res.render('build/index'))
   .use('/api', proxy('0.0.0.0:4000'))
   .use('/socket', proxy('0.0.0.0:8000'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
