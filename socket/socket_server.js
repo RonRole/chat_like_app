1f99be3c18e53a0f5d87b7689fe4b507e35f35cb
RonRole <ronrolenewadress@gmail.com>
2020-04-08 16:25:52 -0700

    sawai

--- socket/socket_server.js
+++ socket/socket_server.js
@@ -5,8 +5,9 @@ console.log(`HOST:${process.env.SOCKET_HOST || 'localhost'}`)
 const http = require('http')
 const server = http.createServer();
 const io = require('socket.io').listen(server)
-server.listen(process.env.SOCKET_PORT || 8000)
+server.listen((process.env.SOCKET_PORT || 8000), (process.env.SOCKET_HOST || 'localhost'))
 
+console.log(server.address)
 //talkRoomID : joinRoomしたメンバーたち
 const currentRoomMembers = {
     0:{
