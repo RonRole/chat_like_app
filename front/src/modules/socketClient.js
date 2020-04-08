b1eb04bf619e1d980e8d2229f019f0695b18a82f
ronrole <ronrolenewadress@gmail.com>
2020-04-08 12:48:42 +0900

    socketの404エラーにとりかかる

--- front/src/modules/socketClient.js
+++ front/src/modules/socketClient.js
@@ -4,4 +4,6 @@
 
 import io from 'socket.io-client'
 
+console.log(`socket client connect to ${process.env.REACT_APP_SOCKET_ADDRESS}`)
+
 export default io.connect(process.env.REACT_APP_SOCKET_ADDRESS)
