a6482d745a583c1f442d6134c2bd747b4a0514e3
ronrole <ronrolenewadress@gmail.com>
2020-04-09 12:44:11 +0900

    socketをデプロイ

--- heroku-express.js
+++ heroku-express.js
@@ -2,12 +2,16 @@ const proxy = require('express-http-proxy');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const io = require('/usr/src/socket/talkRoomMessagesIO')
 
-express()
-  .use(express.static('/usr/src/front/build'))
-//  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
-  .use('/api', proxy('0.0.0.0:4000'))
-  .use('/socket', proxy('0.0.0.0:8000'))
-  .listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
+const app = express().use(express.static('/usr/src/front/build'))
+                      .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
+                      .use('/api', proxy('0.0.0.0:4000'))
+
+const server = require('http').Server(app)
+io.listen(server)
+
+server.listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
+
+
+
