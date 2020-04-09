3809bc24b71a9e745788e968bece837a31d9d2b9
ronrole <ronrolenewadress@gmail.com>
2020-04-09 14:47:57 +0900

    実行環境の変数をいくつか追加

--- heroku-express.js
+++ heroku-express.js
@@ -3,11 +3,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+console.log(`api is served on ${process.env.API_HOST}:${process.env.API_PORT}`)
+console.log(`socket is served on ${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`)
+
 express()
   .use(express.static('/usr/src/front/build'))
 //  .set('views', path.join(__dirname, 'views'))
 //  .set('view engine', 'ejs')
   .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
-  .use('/api', proxy('0.0.0.0:4000'))
-  .use('/socket', proxy('0.0.0.0:8000'))
+  .use('/api', proxy(`${process.env.API_HOST}:${process.env.API_PORT}`))
+  .use('/socket', proxy(`${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`))
   .listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
