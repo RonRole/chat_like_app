b0d83237471ca291dcf98619d58bfc68f759b1d0
ronrole <ronrolenewadress@gmail.com>
2020-04-09 14:15:39 +0900

    socketをどうにかすればよい段階

--- heroku-express.js
+++ heroku-express.js
@@ -2,16 +2,12 @@ const proxy = require('express-http-proxy');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const io = require('/usr/src/socket/talkRoomMessagesIO')
 
-const app = express().use(express.static('/usr/src/front/build'))
-                      .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
-                      .use('/api', proxy('0.0.0.0:4000'))
-
-const server = require('http').Server(app)
-io.listen(server)
-
-server.listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
-
-
-
+express()
+  .use(express.static('/usr/src/front/build'))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
+  .use('/api', proxy('0.0.0.0:4000'))
+  .use('/socket.io', proxy('0.0.0.0:8000'))
+  .listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
--- socket/socket_server.js
+++ socket/socket_server.js
@@ -1,3 +1,4 @@
+console.log("socket server started")
 console.log(`PORT:${process.env.SOCKET_PORT || 8000}`)
 console.log(`HOST:${process.env.SOCKET_HOST || 'localhost'}`)
 
@@ -10,8 +11,71 @@ const server = http.createServer((req,res) => {
 });
 server.listen(process.env.SOCKET_PORT || 8000, process.env.SOCKET_HOST || 'localhost')
 
-const io = require('./talkRoomMessagesIO').listen(server)
+const io = require('socket.io').listen(server)
 io.path(process.env.SOCKET_PATH || '/socket.io')
 console.log(`io:${io}`)
+
 console.log(server.address())
-console.log("socket server started")
+//talkRoomID : joinRoomしたメンバーたち
+const currentRoomMembers = {
+    0:{
+        0:{
+
+        }
+    }
+}
+
+io.sockets.on('connection', socket => {
+    //トークルーム参加
+    socket.on('joinRoom', ({user = {},　roomId}) => {
+        if(!currentRoomMembers[roomId]) {
+            currentRoomMembers[roomId] = {}
+        }
+        currentRoomMembers[roomId][socket.id] = user
+        console.log(`${roomId}に${user.name}が現れた!!!`)
+        console.log(currentRoomMembers[socket.id])
+        socket.join(roomId)
+        io.sockets.in(roomId).emit('receiveMessage', {
+            roomId : roomId,
+            className : "joinRoom",
+            user : user,
+            text : `${user.name}が参加しました`,
+        })
+    })
+    socket.on('leaveRoom', ({user = {}, roomId}) => {
+        console.log(socket.id)
+        if(currentRoomMembers[roomId] && currentRoomMembers[roomId][socket.id]){
+            delete currentRoomMembers[roomId][socket.id]
+        }
+        console.log(`${roomId}から${user.name}が離れました`)
+        console.log(currentRoomMembers[roomId])
+        io.sockets.in(roomId).emit('receiveMessage', {
+            roomId : roomId,
+            className : "leaveRoom",
+            user : user,
+            text : `${user.name}が退出しました`
+        })
+    })
+    //メッセージ送信
+    socket.on('sendMessage',({roomId, user, text}) => {
+        console.log(`${roomId},${user},${text}`)
+        socket.to(roomId).broadcast.emit('receiveMessage', {
+            roomId: roomId, 
+            className:"receiveMessage", 
+            user : user,
+            text : text
+         })
+    })
+    //現在のトークルームのメンバーを取得
+    socket.on('currentUsers', (roomId) => {
+        console.log("get current users")
+        console.log(currentRoomMembers)
+        io.sockets.in(roomId).emit('currentUsers', {
+            roomId : roomId,
+            users : {...currentRoomMembers[roomId]}
+        })
+    })
+})
+
+
+
