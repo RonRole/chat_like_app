2b0ae84b5d546be9d8740c5c1d247da3aa866613
ronrole <ronrolenewadress@gmail.com>
2020-04-09 19:36:24 +0900

    socket設定中

--- heroku-express.js
+++ heroku-express.js
@@ -5,9 +5,11 @@ const PORT = process.env.PORT || 5000
 console.log(`api is served on ${process.env.API_HOST}:${process.env.API_PORT}`)
 console.log(`socket is served on ${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`)
 
-express()
-  .use(express.static('/usr/src/front/build'))
-  .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
-  .use('/api', proxy(`${process.env.API_HOST}:${process.env.API_PORT}`))
-  .use('/socket.io', proxy(`${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`))
-  .listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
+const server = express()
+                  .use(express.static('/usr/src/front/build'))
+                  .get('/', (req, res) => res.sendFile('/usr/src/front/build/index.html'))
+                  .use('/api', proxy(`${process.env.API_HOST}:${process.env.API_PORT}`))
+                  .listen(PORT, () => console.log(`Front Listening on ${ PORT }`))
+
+require('/usr/src/socket/talkRoomMessagesIO')().listen(server)
+
