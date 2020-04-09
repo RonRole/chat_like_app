8eba696f22a53009c599fa0ce2ff9780346cafc7
ronrole <ronrolenewadress@gmail.com>
2020-04-09 11:50:38 +0900

    socketをデプロイ

--- socket/socket_server.js
+++ socket/socket_server.js
@@ -4,12 +4,16 @@ console.log(`HOST:${process.env.SOCKET_HOST || 'localhost'}`)
 
 const http = require('http')
 
-const server = http.createServer();
+const server = http.createServer((req,res) => {
+    if(req.url==='/' && req.method==='GET') {
+        res.write('socket server is running')
+    }
+});
 server.listen(process.env.SOCKET_PORT || 8000, process.env.SOCKET_HOST || 'localhost')
 
-console.log(`io:${io}`)
 const io = require('socket.io').listen(server)
 io.path(process.env.SOCKET_PATH || '/socket.io')
+console.log(`io:${io}`)
 
 console.log(server.address)
 //talkRoomID : joinRoomしたメンバーたち
