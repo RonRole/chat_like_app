6634e609d6f3edfb3ad00b16eff24e1cac283482
RonRole <ronrolenewadress@gmail.com>
2020-04-11 18:52:49 -0700

    ビルドしたフロントにリダイレクトして404エラーになる不具合の修正

--- socket/socket_server.js
+++ socket/socket_server.js
@@ -5,6 +5,8 @@ console.log(`PORT:${process.env.SOCKET_PORT || 8000}`)
 console.log(`HOST:${process.env.SOCKET_HOST || 'localhost'}`)
 
 const http = require('http')
-const server = http.createServer();
+const server = http.createServer((req,res) => {
+    console.log("socket server is active")
+});
 server.listen(process.env.SOCKET_PORT || 8000, process.env.SOCKET_HOST || 'localhost')
 setTalkRoomMessagesIO(server)
