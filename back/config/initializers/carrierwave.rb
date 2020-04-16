2ea3f0fab6e6ce6f3ad80c82a7bbaa7b5311dd45
RonRole <ronrolenewadress@gmail.com>
2020-04-16 07:41:48 -0700

    carrierwaveでフルパスを設定

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,3 +1,3 @@
 CarrierWave.configure do |config|
-    config.asset_host = ENV.fetch('STORAGE_ADDRESS'){'http://localhost:4000'}
+    config.asset_host = ENV.fetch('FILE_UPLOAD_HOST'){'http://localhost:4000'}
 end
