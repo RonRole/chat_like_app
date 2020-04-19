fe546f8f9c83dc5eeb5a912989940406290af532
RonRole <ronrolenewadress@gmail.com>
2020-04-19 07:39:14 -0700

    画像ファイルをクラウドにアップロード

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
     config.asset_host = ENV.fetch('FILE_UPLOAD_HOST'){'http://localhost:4000'}
-    config.fog_provider = 'fog/google'
     config.fog_credentials = {
         :provider                         => 'Google',
         :google_storage_access_key_id     => Rails.application.secrets.google_storage_access_key_id,
