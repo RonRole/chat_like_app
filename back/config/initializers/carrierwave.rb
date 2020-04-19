7cbb3b8406930b478096a62a42346cd4925b396d
RonRole <ronrolenewadress@gmail.com>
2020-04-19 10:26:51 -0700

    production環境でファイルをGCSにアップロードできるよう修正

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
     config.asset_host = ENV.fetch('FILE_UPLOAD_HOST'){'http://localhost:4000'}
+    config.fog_provider='fog/google'
     config.fog_credentials = {
         :provider                         => 'Google',
         :google_storage_access_key_id     => Rails.application.secrets.google_storage_access_key_id,
-        :google_storage_secret_access_key => Rails.application.secrets.google_storage_secret_access_key
+        :google_storage_secret_access_key => Rails.application.secrets.google_storage_secret_access_key,
     }
-    config.fog_directory = ENV["FOG_DIRECTORY"]
+    config.fog_directory = ENV["GCS_FOG_DIRECTORY"]
 end
