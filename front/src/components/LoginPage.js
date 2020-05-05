af615dd866c98a09044c164cd2369e2fd1683507
RonRole <ronrolenewadress@gmail.com>
2020-05-04 19:02:26 -0700

    サインイン・サインアップページのフォームの幅を調整

--- front/src/components/LoginPage.js
+++ front/src/components/LoginPage.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Container, Alert } from 'react-bootstrap'
+import { Container, Alert, Row, Col } from 'react-bootstrap'
 import LoginForm from '../containers/LoginForm'
 
 class LoginPage extends React.Component {
     render() {
         return (
             <Container>
-                {this.props.location.flash ? 
-                    <Alert variant="danger">{this.props.location.flash}</Alert> : ""}
-                <strong>ログインページ</strong>
-                <LoginForm history={this.props.history}/>
+                <Row>
+                    <Col xs={{span:10, offset:1}} sm={{span:8, offset:2}} md={{span:6,offset:3}} lg={{span:4,offset:4}}>
+                        {[this.props.location.flash].filter(e=>e).map(() => <Alert variant="danger">{this.props.location.flash}</Alert>)}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{span:10, offset:1}} sm={{span:8, offset:2}} md={{span:6,offset:3}} lg={{span:4,offset:4}}>
+                        <strong>ログインページ</strong>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{span:10, offset:1}} sm={{span:8, offset:2}} md={{span:6,offset:3}} lg={{span:4,offset:4}}>
+                        <LoginForm history={this.props.history}/>
+                    </Col>
+                </Row>
             </Container>
         )
     }
--- front/src/components/SignUpPage.js
+++ front/src/components/SignUpPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Container} from 'react-bootstrap'
+import { Container, Row, Col} from 'react-bootstrap'
 import SignUpForm from '../containers/SignUpForm'
 
 class SignUpPage extends React.Component {
     render() {
         return (
             <Container>
-                <strong>新規登録</strong>
-                <SignUpForm history={this.props.history}/>
+                <Row>
+                    <Col xs={{span:10, offset:1}} sm={{span:8, offset:2}} md={{span:6,offset:3}} lg={{span:4,offset:4}}>
+                        <strong>新規登録</strong>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{span:10, offset:1}} sm={{span:8, offset:2}} md={{span:6,offset:3}} lg={{span:4,offset:4}}>
+                        <SignUpForm history={this.props.history}/>
+                    </Col>
+                </Row>
             </Container>
         )
     }
