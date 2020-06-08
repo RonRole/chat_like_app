3d80c38027dc2f01937440d01c9fffd902f4dfc4
RonRole <ronrolenewadress@gmail.com>
2020-06-08 09:29:17 -0700

    classコンポーネントを全てfunctionコンポーネントに置き換え

--- front/src/containers/UserInviteButton.js
+++ front/src/containers/UserInviteButton.js
@@ -8,14 +8,10 @@ const UserInviteButton = ({
     userId,
     talkRoomId,
 }) => {
-<<<<<<< HEAD
-    const userIsAlreadyExist = [getTalkRoomById(talkRoomId).userIds, getTalkRoomById(talkRoomId).author_id].flat().some(id => id===userId)
-=======
     const talkRooms = useSelector(state=>state.talkRooms)
     const inviteRoom = talkRooms.ownRooms[talkRoomId] || talkRooms.joinRooms[talkRoomId] || talkRooms.default
     const dispatch = useDispatch()
     const userIsAlreadyExist = [inviteRoom.userIds].flat().some(id => id===userId)
->>>>>>> classコンポーネントを全てfunctionコンポーネントに置き換え
     if(userIsAlreadyExist) {
         return <Button variant="success" disabled>すでに参加しています</Button>
     }
