0374b2e97863255bfb8d89457f5166698ef7bdad
RonRole <ronrolenewadress@gmail.com>
2020-06-15 16:15:48 -0700

    heroku用調整

--- nomlish/nomlish_server.js
+++ nomlish/nomlish_server.js
@@ -7,7 +7,6 @@ const app = express()
 app.get('/translate/:level', (req, res) => {
     res.header('Access-Control-Allow-Origin', process.env.REACT_APP_FRONTEND_ADDRESS)
     res.header('Access-Control-Allow-Credentials','true')
-    const text = req.query.text
     nomlish.translate(text, req.params.level)
             .then(response => res.send(response))
             .catch(err=>console.error(err))
