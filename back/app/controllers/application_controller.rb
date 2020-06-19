d7e4b00f9a20069fd58d137696d82928aa2f3cb8
ronrole <ronrolenewadress@gmail.com>
2020-06-19 10:33:16 +0900

    csrfは杞憂だった

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -3,15 +3,5 @@ class ApplicationController < ActionController::API
     include AuthorizeHelper
     include CurrentUserHelper
 
-    protect_from_forgery
     before_action :is_user_logged_in?
-
-
-    def is_user_logged_in?
-        if !session[:user_id]
-            render_authorize_error
-            return false
-        end
-        return true
-    end
 end
--- back/app/helpers/authorize_helper.rb
+++ back/app/helpers/authorize_helper.rb
@@ -1,4 +1,13 @@
 module AuthorizeHelper
+
+    def is_user_logged_in?
+        if !session[:user_id]
+            render_authorize_error
+            return false
+        end
+        return true
+    end
+    
     def render_authorize_error
         render json:{message:'anauthorize'},status: 401
     end
