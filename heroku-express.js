5c7834204032ba249175f7a33dfcdbf0b4f76ff0
RonRole <ronrolenewadress@gmail.com>
2020-06-23 09:38:55 -0700

    heroku環境でrequest entity too largeが出ることへの対策

--- heroku-express.js
+++ heroku-express.js
@@ -1,19 +1,15 @@
 const proxy = require('express-http-proxy');
 const express = require('express')
-const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 const setTalkRoomMessagesIO = (server) => require('/usr/src/socket/setTalkRoomMessagesIO')(server)
 
 console.log(`api is served on ${process.env.API_HOST}:${process.env.API_PORT}`)
 console.log(`socket is served on ${process.env.SOCKET_HOST}:${process.env.SOCKET_PORT}`)
 console.log(`nomlish is served on ${process.env.NOMLISH_HOST}:${process.env.NOMLISH_PORT}`)
-
-express.uses
+express.urlencoded({
+    limit : '100mb'
+})
 const server = express()
-                .use(bodyParser.urlencoded({
-                    limit: '50mb',
-                    exetended:true
-                }))
                 .use(express.static('/usr/src/front/build'))
                 .use('/api', proxy(`${process.env.API_HOST}:${process.env.API_PORT}`))
                 .use('/nomlish', proxy(`${process.env.NOMLISH_HOST}:${process.env.NOMLISH_PORT}`, {
