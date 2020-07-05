9ec0b6c18ad34a22a6eae6fcb52a4c180418e4bf
RonRole <ronrolenewadress@gmail.com>
2020-07-04 18:02:20 -0700

    スマホで見た時にBGM管理画面のアイテムが改行されていた　ひとまずxs時の幅を小さくして対応

--- front/src/containers/BgmLabelFormChanger.js
+++ front/src/containers/BgmLabelFormChanger.js
@@ -8,16 +8,16 @@ const modeMap = {
     form : 'form'
 }
 
-const LabelFormChanger =  ({
+const BgmUpdateItem =  ({
     bgm,
 }) => {
     const [mode, setMode] = useState(modeMap.label)
     return (
         <Row>
-            <RenderByCondition renderCondition={mode===modeMap.label} WrapWith={Col} xs={10}>
+            <RenderByCondition renderCondition={mode===modeMap.label} WrapWith={Col} xs={9} sm={10}>
                 <BgmManageItem.Label onClick={() => setMode(modeMap.form)} bgmTitle={bgm.title}/>
             </RenderByCondition>
-            <RenderByCondition renderCondition={mode===modeMap.form} WrapWith={Col} xs={10}>
+            <RenderByCondition renderCondition={mode===modeMap.form} WrapWith={Col} xs={9} sm={10}>
                 <BgmManageItem.Form onCancel={() => setMode(modeMap.label)} bgmId={bgm.id} bgmTitle={bgm.title}/>
             </RenderByCondition>
                 <Col xs={1}><BgmManageItem.PlayCursor bgmSrcUrl={bgm.src.url} bgmId={bgm.id}/></Col>
@@ -26,4 +26,4 @@ const LabelFormChanger =  ({
     )
 }
 
-export default LabelFormChanger
+export default BgmUpdateItem
