97100541897cc41c0848fd90d5b9dabbb278f5f6
RonRole <ronrolenewadress@gmail.com>
2020-10-30 23:19:43 -0700

    ノムリッシュ翻訳のリンク復活

--- front/src/components/CurrentRoomPage.js
+++ front/src/components/CurrentRoomPage.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import CurrentRoom from '../containers/CurrentRoom'
-import CurrentRoomTranslateModal from '../containers/CurrentRoomTranslateModal'
 import BgmModalForm from '../containers/BgmModalForm'
 import MessageImageModalForm from '../containers/MessageImageModalForm'
 import Sidebar from './Sidebar'
+import CurrentRoomTranslateModal from '../containers/CurrentRoomTranslateModal'
 
 const MessagesPage = ({
     match,
@@ -20,6 +20,9 @@ const MessagesPage = ({
                 <MessageImageModalForm.ShowLink talkRoomId={match.params.id} className='nav-link d-md-none' to='#'>
                     画像メッセージ送信
                 </MessageImageModalForm.ShowLink>
+                <CurrentRoomTranslateModal.ShowLink>
+                    ノムリッシュ翻訳
+                </CurrentRoomTranslateModal.ShowLink>
             </Sidebar>
             <Sidebar className='d-md-none' small>
                 <Link className = "nav-link" to="/talk_rooms">退出する</Link>
